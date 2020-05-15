4e3f7dbec4de40640f3ea89c35de8d768d05020c
Bro Sand <bsand8813@gmail.com>
2020-05-15 19:54:46 +0800

    penambahan calculation screen

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let prevNumber = '';
 let calculationOperator = '';
 let currentNumber = '0';
 
+let operation = [];
 
 
 const numbers = document.querySelectorAll(".number");
@@ -19,10 +20,19 @@ const updateScreen = (number) => {
 }
 
 
+
+const operationScreen = document.querySelector(".operation-screen");
+
+const updateoperationScreen = (number) =>{
+    operationScreen.value = number;
+}
+
+
 //memanggil angka ketika di klik
 numbers.forEach((number) =>{
     number.addEventListener("click", (event) =>{
         updateScreen(event.target.value);
+        updateoperationScreen(event.target.value);       
     });
 })
 
@@ -41,7 +51,13 @@ const inputNumber = (number) => {
 numbers.forEach((number) =>{
     number.addEventListener("click", (event) =>{
         inputNumber(event.target.value);
-        updateScreen(currentNumber);
+        updateScreen(currentNumber);        
+        if(operation.length < 2){
+            updateoperationScreen(currentNumber);
+        }
+        else{
+            updateoperationScreen(`${prevNumber} ${calculationOperator} ${currentNumber}`);
+        }
     });
 });
 
@@ -52,6 +68,13 @@ const operators = document.querySelectorAll(".operator");
 operators.forEach((operator) =>{
     operator.addEventListener("click",(event) =>{
         inputOperator(event.target.value);
+        operation.push(event.target.value);
+        if(operation.length < 2){
+            updateoperationScreen(currentNumber);
+        }
+        else{
+            updateoperationScreen(`${prevNumber} ${calculationOperator}`);
+        }
     });
 })
 
@@ -61,10 +84,15 @@ operators.forEach((operator) =>{
 // Kosongkan currentNumber.
 
 const inputOperator = (operator) =>{
+    if(calculationOperator === '+' || calculationOperator === '-' || calculationOperator === '*' || calculationOperator === '/'){
+        calculate();
+        updateScreen(currentNumber);
+    }
     if(calculationOperator === ''){
+        operation.push(currentNumber);
         prevNumber = currentNumber;
     }
-   
+        
         calculationOperator = operator;
         currentNumber = '0';
     
@@ -74,13 +102,19 @@ const inputOperator = (operator) =>{
 const equalSign = document.querySelector(".equal-sign");
 
 equalSign.addEventListener("click", () =>{
+    operation.push(currentNumber);
     calculate();
     updateScreen(currentNumber);
+   
 })
 
 //menghitung kalkulasi
 const calculate = () =>{
     let result = '';
+        if (calculationOperator === "/" && currentNumber === 0){
+            result = "Invinity";
+        }
+        else {
         switch (calculationOperator){
             case "+" : 
                 result = parseFloat(prevNumber) +  parseFloat(currentNumber);
@@ -100,6 +134,7 @@ const calculate = () =>{
 
         currentNumber = result;
         calculationOperator = '';
+        }
     }
 
 
@@ -109,12 +144,14 @@ const clearBtn = document.querySelector(".all-clear");
 clearBtn.addEventListener("click",() =>{
     clearAll();
     updateScreen(currentNumber);
+    updateoperationScreen(operation);
 })
 
 const clearAll = () =>{
     prevNumber ='';
     currentNumber = '0';
     calculationOperator = '';
+    operation = [];
 }
 
 //kalkulasi desimal
@@ -122,7 +159,14 @@ const decimal = document.querySelector(".decimal");
 
 decimal.addEventListener("click",(event) =>{
     inputDecimal(event.target.value);
+
     updateScreen(currentNumber);
+    if(operation.length < 2){
+        updateoperationScreen(currentNumber);
+    }
+    else{
+        updateoperationScreen(`${prevNumber} ${calculationOperator}`);
+    }
 })
 
 inputDecimal = (dot) =>{
